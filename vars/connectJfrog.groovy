ae232987fb958baea2cd8315113afeb2da1f8915
sampath-nanda <48028702+sampath-nanda@users.noreply.github.com>
2024-03-11 13:55:43 +0530

    Create connectJfrog.groovy

---
+++ vars/connectJfrog.groovy
@@ -0,0 +1,8 @@
+def call(String IP){
+    withCredentials([usernamePassword(
+            credentialsId: "jfrog",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) 
+    sh "curl --user '$USER':'$PASS' http://${IP}:8081/artifactory"  
+}
