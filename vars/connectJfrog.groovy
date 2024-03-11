9dd2aabb204bc08e0e99b9379b5b3a3270b8b5f7
sampath-nanda <48028702+sampath-nanda@users.noreply.github.com>
2024-03-11 17:13:48 +0530

    Update connectJfrog.groovy

--- vars/connectJfrog.groovy
+++ vars/connectJfrog.groovy
@@ -3,6 +3,7 @@ def call(String IP){
             credentialsId: "jfrog",
             usernameVariable: "USER",
             passwordVariable: "PASS"
-    )]) 
-    sh "curl --user '$USER':'$PASS' http://${IP}:8081/artifactory"  
+    )]) {
+         sh "curl --user '$USER':'$PASS' http://${IP}:8081/artifactory"  
+    }
 }
