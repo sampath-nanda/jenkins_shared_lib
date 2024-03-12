5964443f53b4e3f5c281ba31b2aa2e3c9be8a8b1
sampath-nanda <48028702+sampath-nanda@users.noreply.github.com>
2024-03-12 14:11:42 +0530

    Update pushJfrog.groovy

--- vars/pushJfrog.groovy
+++ vars/pushJfrog.groovy
@@ -3,6 +3,7 @@ def call(String IP){
             credentialsId: "jfrog",
             usernameVariable: "USER",
             passwordVariable: "PASS"
-    )]) 
-    sh "curl -X PUT -u '$USER' -p '$PASS' -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${IP}:8082/artifactory/example-repo-local/"  
-}
+    )]) {
+    sh "curl -X PUT -u '${USER}:${PASS}' -T target/*.jar http://${IP}:8082/artifactory/example-repo-local/"
+    }
+} 
